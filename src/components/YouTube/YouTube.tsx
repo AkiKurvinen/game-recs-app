288c7d1108e207d605a2824aebbe550625d620a5
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2025-06-25 16:23:39 +0300

    init games list

---
+++ src/components/YouTube/YouTube.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import styles from './styles.module.css'
+
+interface YoutubeProps {
+  videoId: string
+  width?: string | number
+  height?: string | number
+  imgSize?: string
+}
+
+const Youtube: React.FC<YoutubeProps> = (props) => {
+  const { videoId, width, height, imgSize } = props
+  const [showVideo, setShowVideo] = useState(false)
+  return (
+    <React.Fragment>
+      {showVideo ? (
+        <iframe
+          width={width || '100%'}
+          height={height || '400px'}
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&showinfo=0`}
+          frameBorder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+          allowFullScreen
+        />
+      ) : (
+        <div
+          className={styles.image}
+          style={{
+            backgroundImage: `url(${`https://img.youtube.com/vi/${videoId}/${
+              imgSize || 'mqdefault'
+            }.jpg`})`,
+            width,
+            height
+          }}
+          onClick={() => setShowVideo(true)}
+        >
+          <div className={styles.playButton} />
+        </div>
+      )}
+    </React.Fragment>
+  )
+}
+export default Youtube
